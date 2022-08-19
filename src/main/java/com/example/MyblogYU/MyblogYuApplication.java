7481d7b07cb17f7d57ebaf8cfe54644dfbbf9fbe
WolfSeven7 <SevenPvc@iCloud.com>
2022-08-19 11:16:38 +0900

    项目的初始化相关

---
+++ src/main/java/com/example/MyblogYU/MyblogYuApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyblogYU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogYuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogYuApplication.class, args);
+	}
+
+}
